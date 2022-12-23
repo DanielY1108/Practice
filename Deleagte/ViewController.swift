6612adcb619b61de6bedcf46c4a53ca14bdf662c
DanielY1108 <scarlet040@gmail.com>
2022-12-23 18:20:18 +0900

    Initial Commit

---
+++ Deleagte/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Deleagte
+//
+//  Created by JINSEOK on 2022/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
